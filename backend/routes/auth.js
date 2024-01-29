8c00e1a96798dc347b51affadcebe7c330f06126
Kumar Sahil <2270168@kiit.ac.in>
2024-01-29 19:23:14 +0530

    7:23pm 29 jan

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "kumarisagoodboy";
 
 //create a user using: Post "localhost:5000/api/auth/createuser".
 router.post(
@@ -28,11 +32,22 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exist" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      //create a new user
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
       /*  .then((user) => res.json(user))
       .catch((err) => {
         console.log(err);
@@ -41,7 +56,8 @@ router.post(
           message: err.message,
         });
       }); */
-      res.json(user);
+      //res.json(user);
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some error ocurred");
