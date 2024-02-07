631ff61166216fef679f72493f2999f743babbc1
Kumar Sahil <2270168@kiit.ac.in>
2024-02-07 19:36:20 +0530

    67

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -76,7 +76,7 @@ const NoteState = (props) => {
   const editNote = async (id, title, description, tag) => {
     // API Call
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: "POST",
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
         "auth-token":
@@ -84,17 +84,21 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({ title, description, tag }),
     });
-    const json = response.json();
-
+    // eslint-disable-next-line
+    const json = await response.json();
+   let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
     for (let index = 0; index < notes.length; index++) {
       const element = notes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
+      
     }
+    setNotes(newNotes);
   };
 
   return (
